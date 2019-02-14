f14adfcf79095e449297ba6a2faeaafa0cd665ad
longan1998 <47628523+longan1998@users.noreply.github.com>
2019-02-14 19:39:16 +0700

    Update Product.java

--- src/assigment4/java/Product.java
+++ src/assigment4/java/Product.java
@@ -13,5 +13,39 @@ public class Product {
         this.quantity = quantity;
     }
 
+    public int getQuantityInProduct() {
+        return this.quantity;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
