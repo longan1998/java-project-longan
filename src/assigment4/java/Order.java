e017a95858f069b2bea35bce87d705d24a8fcf0f
longan <longavth1804009@fpt.edu.vn>
2019-02-14 19:33:18 +0700

    Khoi tao

---
+++ src/assigment4/java/Order.java
@@ -0,0 +1,22 @@
+package vn.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    int id;
+    int total;
+    List<Product> listProduct = new ArrayList<>();
+
+    public void addProduct(Product product, int discount) {
+        if (product != null && product.getQuantityInProduct() > 0) {
+            listProduct.add(product);
+            product.setPrice(product.getPrice() - product.getPrice() * discount / 100);
+
+            System.out.println("Them sp thanh cong" + product.getName());
+        } else {
+            System.out.println("Them sp that bai");
+        }
+    }
+}
+
