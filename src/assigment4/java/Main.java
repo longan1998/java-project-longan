865fb1d239a266d208de7cd924d5a6bc742452c4
longan1998 <47628523+longan1998@users.noreply.github.com>
2019-02-14 20:04:51 +0700

    Update Main.java

--- src/assigment4/java/Main.java
+++ src/assigment4/java/Main.java
@@ -6,20 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        assigment4.java.Product product1 = new vn.java.Product(1,"sp 1", 10000, 5);
-        assigment4.java.Product product2 = new vn.java.Product(2,"sp 2", 20000, 2);
-        assigment4.java.Product product3 = new vn.java.Product(3,"sp 3", 30000, 0);
+        vn.java.Product product1 = new vn.java.Product(1,"sp 1", 10000, 5);
+        vn.java.Product product2 = new vn.java.Product(2,"sp 2", 20000, 2);
+        vn.java.Product product3 = new vn.java.Product(3,"sp 3", 30000, 0);
 
-        assigment4.java.Order order1 = new vn.java.Order();
+        vn.java.Order order1 = new vn.java.Order();
         order1.addProduct(product1, 0);  //0
         order1.addProduct(product2, 20); //1
         order1.addProduct(product3, 0);  //2
 
         // In ra danh sach sp
-        List<assigment4.java.Product> listProduct = order1.getListProduct();
+        List<vn.java.Product> listProduct = order1.getListProduct();
         double total = 0;
         for (int i = 0; i < listProduct.size(); i++) {
-            assigment4.java.Product sp = listProduct.get(i);
+            vn.java.Product sp = listProduct.get(i);
             System.out.println(sp.getName());
             total = total + sp.getPrice()*sp.getQuantityInProduct();
         }
