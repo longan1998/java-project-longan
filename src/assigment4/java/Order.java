c5bf21e1cb787312a56feae484c836d2baf0f1cc
longan1998 <47628523+longan1998@users.noreply.github.com>
2019-02-14 19:41:43 +0700

    Update Order.java

--- src/assigment4/java/Order.java
+++ src/assigment4/java/Order.java
@@ -4,19 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    int id;
-    int total;
-    List<Product> listProduct = new ArrayList<>();
+     int id;
+     int total;
+     List<Product> listProduct = new ArrayList<>();
 
     public void addProduct(Product product, int discount) {
         if (product != null && product.getQuantityInProduct() > 0) {
             listProduct.add(product);
-            product.setPrice(product.getPrice() - product.getPrice() * discount / 100);
+            product.setPrice(product.getPrice() - product.getPrice()*discount/100);
 
             System.out.println("Them sp thanh cong" + product.getName());
         } else {
             System.out.println("Them sp that bai");
         }
     }
-}
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public List<Product> getListProduct() {
+        return listProduct;
+    }
+
+    public void setListProduct(List<Product> listProduct) {
+        this.listProduct = listProduct;
+    }
+
+
+}
